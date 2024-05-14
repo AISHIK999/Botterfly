851f888d2029404b5e7510fe2ae8353923bba8f5
Aishik Mukherjee <aishikm2002@gmail.com>
2024-05-14 09:48:06 +0000

    Remove reddit plugins
    Nuke all reddit relaetd plugins since the API policy changes took place

--- plugins/commands.py
+++ plugins/commands.py
@@ -7,4 +7,4 @@ insult = {"pattern": "\.insult", "incoming": True, "outgoing": True, "from_users
 ytv = {"pattern": "\.ytv", "incoming": True, "outgoing": True, "from_users" : "me"}
 id = {"pattern": "\.id", "incoming": True, "outgoing": True, "from_users" : "me"}
 yta = {"pattern": "\.yta", "incoming": True, "outgoing": True, "from_users" : "me"}
-red = {"pattern": "\.red", "incoming": True, "outgoing": True, "from_users" : "me"}
+
